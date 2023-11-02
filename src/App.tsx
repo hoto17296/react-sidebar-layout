c13f80e80aaa31cd9644cb832a4db72be0b9f3de
Yuki Ishikawa <hoto@y32.jp>
2023-11-02 15:58:15 +0900

    impl Layout component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
 import Layout from './components/Layout'
 
+const generateDummyString = (times: number): string =>
+  [...Array(times)].map(() => 'DUMMY').join(' ')
+
 function App() {
-  return <Layout />
+  return (
+    <Layout>
+      <template slot="sidebar">{generateDummyString(10)}</template>
+      <template slot="content">{generateDummyString(100)}</template>
+    </Layout>
+  )
 }
 
 export default App
--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,5 +1,56 @@
-function Layout() {
-  return <>App</>
+import { useState } from 'react'
+import type { FC, ReactElement, ReactNode } from 'react'
+import style from './Layout.module.css'
+
+function parseSlot(children: ReactElement[]): { [key: string]: ReactNode } {
+  const slot: { [key: string]: ReactNode } = {}
+  children.forEach((child) => {
+    if (child.type === 'template' && child.props.slot) {
+      slot[child.props.slot] = child.props.children
+    }
+  })
+  return slot
+}
+
+interface LayoutProps {
+  children: ReactElement[]
+}
+
+const Layout: FC<LayoutProps> = ({ children }) => {
+  const [showSidebar, setShowSidebar] = useState<boolean>(
+    document.body.clientWidth > 768
+  )
+  const [inTransition, setInTransition] = useState<boolean>(false)
+
+  const slot = parseSlot(children)
+
+  const sidebarClassNames = [
+    style.sidebar,
+    showSidebar ? style.show : style.hide,
+  ]
+  if (inTransition) sidebarClassNames.push(style.inTransition)
+
+  return (
+    <div className={style.container}>
+      <div
+        className={sidebarClassNames.join(' ')}
+        onTransitionEnd={() => setInTransition(false)}
+      >
+        <button
+          className={style.toggleButton}
+          onClick={() => {
+            setInTransition(true)
+            setShowSidebar(!showSidebar)
+          }}
+        />
+        <div>{slot.sidebar}</div>
+      </div>
+      <div className={style.content}>
+        <div>{slot.content}</div>
+        <div className={style.overlay} />
+      </div>
+    </div>
+  )
 }
 
 export default Layout
