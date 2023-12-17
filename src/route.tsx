0930226520223c2aafb48a9892233b0165f1b8a6
Yuki Ishikawa <hoto@y32.jp>
2023-12-17 11:45:37 +0900

    bugfix

--- src/route.tsx
+++ src/route.tsx
@@ -1,9 +1,12 @@
 import { createBrowserRouter } from 'react-router-dom'
 import RootPage from './pages/RootPage'
 
-export const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <RootPage />,
-  },
-])
+export const router = createBrowserRouter(
+  [
+    {
+      path: '/',
+      element: <RootPage />,
+    },
+  ],
+  { basename: import.meta.env.BASE_URL }
+)
